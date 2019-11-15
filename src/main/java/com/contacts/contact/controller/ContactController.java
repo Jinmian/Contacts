adef5a4f856b65c93325f43eebd74397b04dd9b8
jinmian.zheng <jinmian.zheng@acensi.fr>
2019-11-15 16:43:39 +0100

    work in progress

--- src/main/java/com/contacts/contact/controller/ContactController.java
+++ src/main/java/com/contacts/contact/controller/ContactController.java
@@ -5,7 +5,6 @@ import com.contacts.contact.services.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
